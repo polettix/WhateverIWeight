3430636eac211be47e32d4bdee670e8fe681f9a6
Flavio Poletti <flavio@polettix.it>
2011-08-27 02:21:24 +0200

    advanced in frontend

--- www/lib/whatever.pm
+++ www/lib/whatever.pm
@@ -9,9 +9,35 @@ use Dancer;
 use Dancer::Plugin::FlashNote qw( flash );
 our $VERSION = '0.1';
 
+use DotCloudStuff qw< get_nosqldb_handle >;
+
+$redis = get_nosqldb_handle();
+
 get '/' => sub {
-   template 'index';
+   my %p;
+   session(username => params->{username}) if params->{username};
+   $p{weights} = get_weights_of(session('username'))
+      if session('username');
+   $p{users} = get_users();
+   template 'index', \%p;
 };
+
+get '/show/:username' => sub {
+   session(username => params->{username});
+   redirect '/';
+};
+
+post '/record' => sub {
+
+};
+
+sub get_weights_of {
+   
+}
+
+sub get_users {
+   return [ $redis->smembers('users') ];
+}
 
 1;
 __END__
